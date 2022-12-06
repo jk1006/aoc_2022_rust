b9472bc6800890352b897489b6db264d97c4901b
Julius Klotz <julius.klotz@posteo.de>
2022-12-06 13:07:35 +0100

    Finished part 5, solved part 6

--- src/day5.rs
+++ src/day5.rs
@@ -1,57 +1,106 @@
 pub fn run() {
-    let input = split_input(include_str!("puzzle_inputs/day1.txt"));
-    println!("Solution day1_part1: {}", solve_part1(input.clone()));
-    println!("Solution day1_part2: {}", solve_part2(input.clone()));
+    let input = split_input(include_str!("puzzle_inputs/day5.txt"));
+    println!("Solution day5_part1: {}", solve_part1(input.clone()));
+    println!("Solution day5_part2: {}", solve_part2(input.clone()));
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Move {
     amount: usize,
     from: usize,
     to: usize,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Stack {
-    crates: Vec<char>,
+    crates: Vec<Vec<char>>,
     moves: Vec<Move>,
 }
 
-impl Stack {
-    fn add_move(&mut self, mv: Move) {
+impl Stack { fn add_move(&mut self, mv: Move) {
         self.moves.push(mv);
     }
 }
 
-fn solve_part1(input: Vec<usize>) -> String {
-
-    "".to_string()
+fn solve_part1(mut input: Stack) -> String {
+    for mv in input.moves {
+        let vec_to_process = input.crates[mv.from - 1].clone();
+        input.crates[mv.to - 1].append(&mut vec_to_process[vec_to_process.len()-mv.amount..].into_iter().rev().map(|c| *c).collect());
+        if mv.amount == input.crates[mv.from - 1].len() {
+            input.crates[mv.from - 1].clear();
+        } else {
+        input.crates[mv.from - 1] = vec_to_process[..vec_to_process.len()-mv.amount].to_vec();
+        }
+    }
+    let mut result: String = "".to_string();
+    for cr in input.crates {
+       result.push(cr.into_iter().last().unwrap() );
+    }
+    result
 }
 
-fn solve_part2(mut input: Vec<usize>) -> usize {
-    input.sort();
-    return input.iter().rev().take(3).sum();
+fn solve_part2(mut input: Stack) -> String {
+    for mv in input.moves {
+        let vec_to_process = input.crates[mv.from - 1].clone();
+        input.crates[mv.to - 1].append(&mut vec_to_process[vec_to_process.len()-mv.amount..].into_iter().map(|c| *c).collect());
+        if mv.amount == input.crates[mv.from - 1].len() {
+            input.crates[mv.from - 1].clear();
+        } else {
+        input.crates[mv.from - 1] = vec_to_process[..vec_to_process.len()-mv.amount].to_vec();
+        }
+    }
+    let mut result: String = "".to_string();
+    for cr in input.crates {
+       result.push(cr.into_iter().last().unwrap() );
+    }
+    result
 }
 
-fn split_input(input: &str) -> Vec<Stack> {
-    let input_iter = input.split("\n\n");
+fn split_input(input: &str) -> Stack {
+    let mut input_iter = input.split("\n\n");
     let splitted: &str = input_iter.next().unwrap();
     let moves = input_iter.next().unwrap().split("\n")
         .filter(|l| !l.is_empty())
-        .map(create_move);
-
+        .map(create_move).collect();
+    let mut lines: Vec<&str> = splitted.split("\n").collect();
+    let last_line = lines.remove(lines.len() - 1);
+    let number_of_cols: usize = last_line.split("   ").last().unwrap().trim().parse::<usize>().unwrap();
+    let mut crates = vec![];
+    for _ in 0..number_of_cols {
+        crates.push(Vec::new());
+    }
+    for line in lines {
+        for (i, c) in line.chars()
+            .enumerate() {
+                if c.is_ascii_alphabetic() && c != ' ' {
+                    match i {
+                        1 => crates[0].insert(0,c),
+                        5 => crates[1].insert(0, c),
+                        9 => crates[2].insert(0, c),
+                        13 => crates[3].insert(0, c),
+                        17 => crates[4].insert(0, c),
+                        21 => crates[5].insert(0, c),
+                        25 => crates[6].insert(0, c),
+                        29 => crates[7].insert(0, c),
+                        33 => crates[8].insert(0, c),
+                        _ => println!("{}, {}", c, i),
+                    }
+                }
+            }
+    }
     Stack {
+        crates,
         moves,
     }
 }
 
 fn create_move(input: &str) -> Move {
-    let move_iter = input.split(" from ");
+    let mut move_iter = input.split(" from ");
     let first_part = move_iter.next().unwrap();
-    let amount: usize = first_part[5..first_part.len() +1].parse::<usize>().unwrap();
+    let amount: usize = first_part[5..first_part.len()].parse::<usize>().unwrap();
     let second_part = move_iter.next().unwrap();
     let from = second_part[0..1].parse::<usize>().unwrap();
-    let to = second_part[6..7].parse::<usize>().unwrap();
+    let to = second_part[5..6].parse::<usize>().unwrap();
 
     Move {
         amount,
@@ -73,14 +122,14 @@ move 2 from 2 to 1
 move 1 from 1 to 2";
     
     #[test]
-    fn day1_part1() {
+    fn day5_part1() {
         let input = split_input(TEST_INPUT);
         assert_eq!(solve_part1(input), "CMZ".to_string());
     }
 
     #[test]
-    fn day1_part2() {
+    fn day5_part2() {
         let input = split_input(TEST_INPUT);
-        //assert_eq!(solve_part2(input), 45000);
+        assert_eq!(solve_part2(input), "MCD".to_string());
     }
 }
---
+++ src/day6.rs
@@ -0,0 +1,101 @@
+pub fn run() {
+    let input = split_input(include_str!("puzzle_inputs/day6.txt"));
+    println!("Solution day6_part1: {}", solve_part1(input.clone()));
+    println!("Solution day6_part2: {}", solve_part2(input.clone()));
+}
+
+fn solve_part1(input: Vec<char>) -> usize {
+    let mut last_4 = Vec::new();
+    for (i, c) in input.into_iter().enumerate() {
+       last_4.push(c); 
+       if last_4.len() == 5 {
+           last_4.remove(0);
+           let mut tmp = last_4.clone();
+           tmp.sort();
+           tmp.dedup();
+           if tmp.len() == 4 {
+               return i + 1;
+           }
+       }
+    }
+    0
+}
+
+fn solve_part2(mut input: Vec<char>) -> usize {
+    let mut last_14 = Vec::new();
+    for (i, c) in input.into_iter().enumerate() {
+       last_14.push(c); 
+       if last_14.len() == 15 {
+           last_14.remove(0);
+           let mut tmp = last_14.clone();
+           tmp.sort();
+           tmp.dedup();
+           if tmp.len() == 14 {
+               return i + 1;
+           }
+       }
+    }
+    0
+}
+
+fn split_input(input: &str) -> Vec<char> {
+    return input
+        .chars()
+        .collect();
+}
+
+mod tests {
+    use crate::day6::*;
+    const TEST_INPUT1: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+    const TEST_INPUT2: &str = "nppdvjthqldpwncqszvftbrmjlhg";
+    const TEST_INPUT3: &str = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    const TEST_INPUT4: &str = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+
+    #[test]
+    fn day6_part1_1() {
+        let input = split_input(TEST_INPUT1);
+        assert_eq!(solve_part1(input), 5);
+    }
+
+    #[test]
+    fn day6_part1_2() {
+        let input = split_input(TEST_INPUT2);
+        assert_eq!(solve_part1(input), 6);
+    }
+    #[test]
+    fn day6_part1_3() {
+        let input = split_input(TEST_INPUT3);
+        assert_eq!(solve_part1(input), 10);
+    }
+    #[test]
+    fn day6_part1_4() {
+        let input = split_input(TEST_INPUT4);
+        assert_eq!(solve_part1(input), 11);
+    }
+    #[test]
+    fn day6_part2_1() {
+        let input = split_input("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+        assert_eq!(solve_part2(input), 19);
+    }
+
+    #[test]
+    fn day6_part2_2() {
+        let input = split_input("bvwbjplbgvbhsrlpgdmjqwftvncz");
+        assert_eq!(solve_part2(input), 23);
+    }
+    #[test]
+    fn day6_part2_3() {
+        let input = split_input("nppdvjthqldpwncqszvftbrmjlhg");
+        assert_eq!(solve_part2(input), 23);
+    }
+    #[test]
+    fn day6_part2_4() {
+        let input = split_input("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+        assert_eq!(solve_part2(input), 29);
+    }
+    #[test]
+    fn day6_part2_5() {
+        let input = split_input("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+        assert_eq!(solve_part2(input), 26);
+    }
+}
