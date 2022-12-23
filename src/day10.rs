9e26a743298a546746cc4e748ecf04b67278b7cf
Julius Klotz <julius.klotz@posteo.de>
2022-12-23 09:28:07 +0100

    finished day 10

--- src/day10.rs
+++ src/day10.rs
@@ -16,7 +16,7 @@ impl Instruction {
 pub fn run() {
     let input = split_input(include_str!("puzzle_inputs/day10.txt"));
     println!("Solution day10_part1: {}", solve_part1(input.clone()));
-    println!("Solution day10_part2: {}", solve_part2(input));
+    println!("Solution day10_part2:\n{}", solve_part2(input));
 }
 
 fn solve_part1(input: Vec<Instruction>) -> i32 {
@@ -53,30 +53,41 @@ fn solve_part1(input: Vec<Instruction>) -> i32 {
 }
 
 fn solve_part2(mut input: Vec<Instruction>) -> String {
-   let board = vec![vec!['.';40];6]; 
-   let mut current_cycle = 1;
-   let mut cycles_instruction_finished = 0;
-   let mut sprite_middle = 1;
-   let mut value_to_add = 0;
-   for instruction in input {
-       match instruction {
-        Instruction::Add(value) => {
-            value_to_add = value;
-            cycles_instruction_finished = 2;
-            current_cycle += 1;
-        },
-        Instruction::Noop => {
-           if cycles_instruction_finished > 0 {
-            cycles_instruction_finished -= 1;
-           }
+    let mut board = vec![vec!['.'; 40]; 6];
+    let mut sprite_middle = 1;
+    let mut no_current_instruction = true;
+    let mut current_instruction: (Instruction, usize) = (Instruction::Noop, 0);
+    for cycle in 0..240 {
+        if no_current_instruction {
+            let instr = input.remove(0);
+            current_instruction = (instr, 2);
+            no_current_instruction = false;
         }
-       } 
-       if cycles_instruction_finished == 0 {
-        
-       }
-   }
-
-    "".to_string()
+        println!("CYCLE: {}", cycle);
+        println!("Currenct instruction: {:?}", current_instruction);
+        println!("sprite middle: {}", sprite_middle);
+        if (sprite_middle - 1..=sprite_middle + 1).contains(&(cycle % 40)) {
+            board[(cycle / 40) as usize][(cycle % 40) as usize] = '#'
+        }
+        match current_instruction.0 {
+            Instruction::Add(value) => {
+                current_instruction.1 -= 1;
+                if current_instruction.1 == 0 {
+                    sprite_middle += value;
+                    no_current_instruction = true;
+                }
+            }
+            Instruction::Noop => {
+                no_current_instruction = true;
+            }
+        }
+    }
+    let mut result = "".to_string();
+    for line in board {
+        result.push_str(&line.iter().cloned().collect::<String>());
+        result.push_str("\n");
+    }
+    result
 }
 
 fn split_input(input: &str) -> Vec<Instruction> {
@@ -258,11 +269,12 @@ noop";
     fn day10_part2() {
         let input = split_input(TEST_INPUT);
         let expected = "##..##..##..##..##..##..##..##..##..##..
-        ###...###...###...###...###...###...###.
-        ####....####....####....####....####....
-        #####.....#####.....#####.....#####.....
-        ######......######......######......####
-        #######.......#######.......#######.....";
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+";
         assert_eq!(solve_part2(input), expected.to_string());
     }
 }
